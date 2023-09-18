c42741c8339f08404a0a2da333942ef81148e53c
Elliot Trapp <elliot@trapp.codes>
2023-09-17 19:08:29 -0700

    started on exp

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import About from "@/components/about";
 import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
+import Projects from "@/components/projects";
+import Skills from "@/components/skills";
+import Experience from "@/components/experience";
+import Contact from "@/components/contact";
 
 export default function Home() {
   return (
@@ -8,6 +12,10 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </main>
   );
 }
---
+++ components/experience.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+
+export default function Experience() {
+  return (
+    <section>
+      <SectionHeading>Experience</SectionHeading>
+    </section>
+  );
+}
